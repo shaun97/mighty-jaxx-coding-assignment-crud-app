77b369fbe885be6b8d869f79991b5df9593f6b52
Shaun <xshaun97@gmail.com>
2022-10-24 14:09:40 +0800

    update export

--- frontend/firebase/clientApp.js
+++ frontend/firebase/clientApp.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 
-
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -10,6 +9,4 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
 };
 
-const clientApp = initializeApp(firebaseConfig);
-
-export default clientApp;
+export const clientApp = initializeApp(firebaseConfig);
